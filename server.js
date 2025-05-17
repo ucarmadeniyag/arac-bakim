525f960ebee6fa2d549cbecbd994f518cf28a0f3
ucarmadeniyag <ucarmadeniyag@hotmail.com>
2025-05-17 10:51:15 +0300

    Server.js ve index.html güncellendi, yeni API eklendi

--- server.js
+++ server.js
@@ -64,7 +64,26 @@ app.post('/api/bakimlar/:plaka', (req, res) => {
     return res.status(400).json({ message: 'Eksik bilgi' });
   }
 
-  fs.readFile(DATA_FILE, 'utf8', (err, data_
+  fs.readFile(DATA_FILE, 'utf8', (err, data) => {
+    if (err) return res.status(500).json({ message: 'Sunucu hatası' });
+
+    let json;
+    try {
+      json = JSON.parse(data);
+    } catch {
+      return res.status(500).json({ message: 'Veri okuma hatası' });
+    }
+
+    if (!json[plaka]) json[plaka] = [];
+    json[plaka].push({ tarih, islem });
+
+    fs.writeFile(DATA_FILE, JSON.stringify(json, null, 2), (err) => {
+      if (err) return res.status(500).json({ message: 'Kayıt edilemedi' });
+      res.json({ message: 'Kayıt başarılı' });
+    });
+  });
+});
+
 // Tüm araçların bakım kayıtlarını listele
 app.get('/api/bakimlar', (req, res) => {
   fs.readFile(DATA_FILE, 'utf8', (err, data) => {
